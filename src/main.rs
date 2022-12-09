2cb701dece99b07f143a01280edc064378743c88
name1e5s <name1e5s@qq.com>
2022-12-09 22:26:45 +0800

    config

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use std::path::Path;
+use serde::Deserialize;
+use std::{collections::HashMap, fs::read_to_string, path::Path};
 
 mod api;
 mod headers;
 mod writer;
 
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    identity: String,
+    authorization: String,
+    blade_auth: String,
+    sites: HashMap<String, String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    headers::set_identity("".to_string());
-    headers::set_authorization("".to_string());
-    headers::set_blade_auth("".to_string());
-    let result = api::list_all_students("1558997828460613635".to_string()).await?;
-    println!(
-        "Hello, world! {:#?}",
-        result
-    );
-    writer::write_file(Path::new("r.csv"), result)?;
+    let config: Config = toml::from_str(&read_to_string("config.toml")?)?;
+    headers::set_identity(config.identity);
+    headers::set_authorization(config.authorization);
+    headers::set_blade_auth(config.blade_auth);
+    for (dest, site_id) in config.sites {
+        let result = api::list_all_students(site_id).await?;
+        println!("{}: {:#?}", dest, result);
+        writer::write_file(Path::new(&format!("{}.csv", dest)), result)?;
+    }
     Ok(())
 }
